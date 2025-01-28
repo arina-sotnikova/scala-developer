0de0cfc2796b47dae7f2a9d3e0d0abc981cd8a74
Arina Sotnikova <lepilova.arina@gmail.com>
2025-01-28 20:16:09 +0300

    Added the 1st version of hw2

--- src/main/scala/ru/otus/module1/DataCollection1/homework/homework1.scala
+++ src/main/scala/ru/otus/module1/DataCollection1/homework/homework1.scala
@@ -1,18 +1,24 @@
 package ru.otus.module1.DataCollection1
-/*
-class BallsExperiment {
 
-  def isFirstBlackSecondWhite(): Boolean = {
-    ???
+import scala.util.Random
+
+class BallsExperiment {
+  private val boxWithBalls: List[Int] = Random.shuffle(List(1, 1, 1, 0, 0, 0))
+
+  def isFirstBlackSecondWhite: Boolean = {
+    val firstTwoBalls = boxWithBalls.take(2)
+    firstTwoBalls.head == 0 && firstTwoBalls.last == 1
   }
+
+  def isFirstBlack: Boolean = boxWithBalls.head == 0
 }
 
 object BallsTest {
   def main(args: Array[String]): Unit = {
     val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
-    val countOfExperiments = ???
+    val listOfExperiments: List[BallsExperiment] = List.fill(count)(new BallsExperiment())
+    val countOfExperiments = listOfExperiments.map(_.isFirstBlackSecondWhite)
     val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
-    println(countOfPositiveExperiments / count)
+    println(countOfPositiveExperiments / listOfExperiments.map(_.isFirstBlack).count(_ == true))
   }
-}*/
+}
